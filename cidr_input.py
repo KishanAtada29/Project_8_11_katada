6f813f34c1fee606a7fd8cd936654563c0d906dd
Kishan Atada <katada@student.cscc.edu>
2026-07-02 23:22:02 -0400

    Moved the get result function from function to cidr input file

--- cidr_input.py
+++ cidr_input.py
@@ -5,9 +5,30 @@
     Starter Code Information: No starter code was used.
     Date: June 23, 2026
 """
-
-import functions as f
+import subnetmask_calculator as sc
+from functions import Functions
 import cidr_desc as cd
+
+def get_result(cidr,desc):
+        f = Functions()
+        """
+        Display the calculated CIDR information.
+
+        Args:
+            cidr: An integer from 0 to 32.
+            desc: A description of the CIDR prefix.
+        """
+        if cidr == 31:
+            ip = 2
+        elif cidr == 32:
+            ip = 1
+        else: 
+            ip = f.usable_ip(cidr)
+            
+        print(f'CIDR: /{cidr}')
+        print(f'Number of useable host: {ip}')
+        print(f'Subnet Mask: {".".join(map(str,(sc.subnetmask_calculator(cidr))))}')
+        print('Description: ' + desc)
 
 def cidr_calculator():
     """
@@ -23,7 +44,8 @@ def cidr_calculator():
 
 
     print('')
-    f.get_result(cidr, cd.cidr_desc(cidr))
+    get_result(cidr, cd.cidr_desc(cidr))
    
    
 
+    
